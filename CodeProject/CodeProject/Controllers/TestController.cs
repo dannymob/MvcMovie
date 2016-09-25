459c8c1c18a84a8d2e86a13ff507cc703a7e4a29
dannymob <dannym@moblin.com>
2016-09-25 18:20:34 +0300

    Lab 5 - Understand strongly typed Views

--- CodeProject/CodeProject/Controllers/TestController.cs
+++ CodeProject/CodeProject/Controllers/TestController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CodeProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,11 +7,44 @@ using System.Web.Mvc;
 
 namespace CodeProject.Controllers
 {
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+
+        public override string ToString()
+        {
+            return this.CustomerName + "|" + this.Address;
+        }
+    }
+
     public class TestController : Controller
     {
+
+        public Customer GetCustomer()
+        {
+            Customer c = new Customer();
+            c.CustomerName = "John Doh";
+            c.Address = "Area 51";
+            return c;
+            
+        }
+
         public string GetString()
         {
             return "Hello World is old now. It&rsquo;s time for wassup bro ;)"; 
+        }
+
+        public ActionResult GetView()
+        {
+            Employee emp = new Employee();
+            emp.FirstName = "Jesus";
+            emp.LastName = "Miguel";
+            emp.Salary = 20000;
+
+            //ViewBag.Employee = emp;
+            //ViewData["Employee"] = emp;
+            return View("MyView", emp);
         }
 
         // GET: Test
