f4980a41558b7f85436ac27c99ee63bb6720786f
dannymob <dannym@moblin.com>
2016-10-10 18:11:36 +0300

    new view with hreader and footer

---
+++ CodeProject/ViewModel/EmployeeListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CodeProject.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeProject.ViewModel
+{
+    public class EmployeeListViewModel:BaseViewModel
+    {
+        public List <EmployeeViewModel> Employees { get; set; }
+    }
+}
